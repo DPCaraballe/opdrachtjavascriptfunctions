6935cae935a01b2cef0123f7f774dcbf74407dcf
DPCaraballe <84527568+DPCaraballe@users.noreply.github.com>
2021-06-15 19:18:20 +0200

    Add files via upload

---
+++ opdrachtjavascriptfunctions/functions.js
@@ -0,0 +1,24 @@
+ 
+const makePizza = function() {
+    console.log("Make dough");
+    console.log("Make sauce");
+    console.log("Slice toppings");
+    console.log("Shred cheese");
+    console.log("Roll dough");
+    console.log("Add sauce and toppings");
+    console.log("Bake at very high temp in oven");
+}
+
+makePizza();
+
+const makeSushi = function() {
+    console.log("Cook rice");
+    console.log("Carefully slice fish");
+    console.log("Choose other toppings");
+    console.log("Roll sushi");
+    console.log("Slice");
+    console.log("Make various sushi");
+    console.log("Serve with wasabi");
+}
+
+makeSushi();
